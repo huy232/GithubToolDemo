0876d77e126f7c89b16291dd0022f2a68a9fbd23
Anh Le <lhanhfire1999@gmail.com>
2021-04-10 16:36:11 +0700

    Add-to-cart-product-from-db

--- Template/_cart-template.php
+++ Template/_cart-template.php
@@ -1,17 +1,4 @@
 <!-- Shopping cart section  -->
-<?php
-    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        if (isset($_POST['delete-cart-submit'])){
-            $deletedrecord = $Cart->deleteCart($_POST['item_id']);
-        }
-        
-        // save for later
-        if (isset($_POST['wishlist-submit'])){
-            $Cart->saveForLater($_POST['item_id']);
-        }
-    }
-?>
-
 <section id="cart" class="py-3">
     <div class="container-fluid w-75">
         <h5 class="font-baloo font-size-20">Shopping Cart</h5>
@@ -48,19 +35,12 @@
                                 <!-- product qty -->
                                     <div class="qty d-flex pt-2">
                                         <div class="d-flex font-rale w-25">
-                                            <button class="qty-up border bg-light" data-id="<?php echo $item['item_id'] ?? '0'; ?>"><i class="fas fa-angle-up"></i></button>
-                                            <input type="text" data-id="<?php echo $item['item_id'] ?? '0'; ?>" class="qty_input border px-2 w-100 bg-light" disabled value="1" placeholder="1">
-                                            <button data-id="<?php echo $item['item_id'] ?? '0'; ?>" class="qty-down border bg-light"><i class="fas fa-angle-down"></i></button>
+                                            <button class="qty-up border bg-light" data-id="pro1"><i class="fas fa-angle-up"></i></button>
+                                            <input type="text" data-id="pro1" class="qty_input border px-2 w-100 bg-light" disabled value="1" placeholder="1">
+                                            <button data-id="pro1" class="qty-down border bg-light"><i class="fas fa-angle-down"></i></button>
                                         </div>
-                                        <form method="post">
-                                          <input type="hidden" value="<?php echo $item['item_id'] ?? 0; ?>" name="item_id">
-                                          <button type="submit" name="delete-cart-submit" class="btn font-baloo text-danger px-3 border-right">Delete</button>
-                                        </form>
-                                        <form method="post">
-                                          <input type="hidden" value="<?php echo $item['item_id'] ?? 0; ?>" name="item_id">
-                                          <button type="submit" name="wishlist-submit" class="btn font-baloo text-danger">Save for Later</button>
-                                        </form>
-                                        
+                                        <button type="submit" class="btn font-baloo text-danger px-3 border-right">Delete</button>
+                                        <button type="submit" class="btn font-baloo text-danger">Save for Later</button>
                                     </div>
                                 <!-- !product qty -->
 
@@ -68,7 +48,7 @@
 
                             <div class="col-sm-2 text-right">
                                 <div class="font-size-20 text-danger font-baloo">
-                                  <span class="product_price" data-id="<?php echo $item['item_id'] ?? '0'; ?>"><?php echo $item['item_price'] ?? 0; ?></span> VND
+                                  <span class="product_price"><?php echo $item['item_price'] ?? 0; ?></span> VND
                                 </div>
                             </div>
                         </div>
@@ -84,8 +64,9 @@
                     <div class="sub-total border text-center mt-2">
                         <h6 class="font-size-12 font-rale text-success py-3"><i class="fas fa-check"></i> Your order is eligible for FREE Delivery.</h6>
                         <div class="border-top py-4">
-                          <h5 class="font-baloo font-size-20">Subtotal ( <?php echo isset($subTotal) ? count($subTotal) : 0; ?> item):&nbsp; <span class="text-danger"><span class="text-danger" id="deal-price"><?php echo isset($subTotal) ? $Cart->getSum($subTotal) : 0; ?></span> VND</span> </h5>
-                          <button type="submit" class="btn btn-warning mt-3">Proceed to Buy</button>
+                             <h5 class="font-baloo font-size-20">Subtotal ( <?php echo count($subTotal) ?? 0; ?> item):&nbsp; 
+                              <span class="text-danger"><span class="text-danger" id="deal-price"><?php echo isset($subTotal) ? $Cart->getSum($subTotal) : 0; ?> VND</span> </span> </h5>
+                            <button type="submit" class="btn btn-warning mt-3">Proceed to Buy</button>
                         </div>
                     </div>
                 </div>

cd9611e2e2452515f3fad7e9ac6d51f66e8d9caa
Anh Le <lhanhfire1999@gmail.com>
2021-04-10 16:25:14 +0700

    Fletch-product-from-Database

--- Template/_new-phones.php
+++ Template/_new-phones.php
@@ -2,14 +2,6 @@
 <?php
   shuffle($product_shuffle);
 
-    // request method post
-  if($_SERVER['REQUEST_METHOD'] == "POST"){
-    if (isset($_POST['new_phones_submit'])){
-      // call method addToCart
-      $Cart->addToCart($_POST['user_id'], $_POST['item_id']);
-      }
-  }
-
 ?>    
 
         <section id="new-phones">
@@ -23,7 +15,7 @@
             <?php foreach ($product_shuffle as $item){?>
               <div class="item py-2 bg-light">
                   <div class="product font-rale">
-                       <a href="<?php printf('%s?item_id=%s', 'product.php',  $item['item_id']); ?>"><img src="<?php echo $item['item_image'] ?? "./assets/products/1.png"; ?>" alt="product1" class="img-fluid"></a>
+                       <a href="#"><img src="<?php echo $item['item_image'] ?? "./assets/products/1.png"; ?>" alt="product1" class="img-fluid"></a>
                       <div class="text-center">
                           <h6><?php echo  $item['item_name'] ?? "Unknown";  ?></h6>
                           <div class="rating text-warning font-size-12">
@@ -36,17 +28,7 @@
                           <div class="price py-2">
                               <span><?php echo $item['item_price'] ?? '0' ; ?> VND</span>
                           </div>
-                          <form method="post">
-                            <input type="hidden" name="item_id" value="<?php echo $item['item_id'] ?? '1'; ?>">
-                            <input type="hidden" name="user_id" value="<?php echo 1; ?>">
-                             <?php
-                              if (in_array($item['item_id'], $Cart->getCartId($product->getData('cart')) ?? [])){
-                                  echo '<button type="submit" disabled class="btn btn-success font-size-12">In the Cart</button>';
-                              }else{
-                                  echo '<button type="submit" name="top_sale_submit" class="btn btn-warning font-size-12">Add to Cart</button>';
-                              }
-                            ?>
-                          </form>
+                          <button type="submit" class="btn btn-warning font-size-12">Add to Cart</button>
                       </div>
                   </div>
 

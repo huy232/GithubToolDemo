52559e1b7189c1cec71fa17e2e22bd46098a31da
Anh Le <lhanhfire1999@gmail.com>
2021-04-10 16:11:38 +0700

    Connection-to-database

--- header.php
+++ header.php
@@ -4,15 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mobile Shopee</title>
+
+
     <!-- Bootstrap CDN-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
     <!-- Owl Carousel CDN-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" integrity="sha512-tS3S5qG0BlhnQROyJXvNjeEM4UpMXHrQfTGmbQ1gKmelCxlSEBUaxhRBj/EFTzpbP4RVSrpEikbmdJobCvhE3g==" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css" integrity="sha512-OTcub78R3msOCtY3Tc6FzeDJ8N9qvQn1Ph49ou13xgA9VsH9+LRxoFU6EqLhW4+PKRfU+/HReXmSZXHEkpYoOA==" crossorigin="anonymous" />
+
     <!-- Font Awesome CDN-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
     <!-- Custom CSS file-->
     <link rel="stylesheet" href="style.css">
+
+    <?php 
+      // require functions.php file
+      require('functions.php');
+    ?>
+
 </head>
 <body>
     <!-- Start #header-->
@@ -23,6 +33,7 @@
                 <a href="#" class="px-3 border-right text-dark">Wishlist (0)</a>
             </div>
         </div>
+
         <!--Primary Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark color-second-bg">
             <a class="navbar-brand" href="#">Mobile Shopee</a>
@@ -54,12 +65,16 @@
                   <a href="#" class="py-2 rounded-pill color-primary-bg">
                     <span class="font-size-16 px-2 text-white"><i class="fas fa-shopping-cart"></i></span>
                     <span class="px-3 py-2 rounded-pill text-dark bg-light"></span>
+
                   </a>
               </form>
             </div>
           </nav>
           <!--Primary Navigation-->
+
+
     </header>
     <!-- Start #header-->
+
     <!-- Start #main-site-->
     <main id="main-site">
